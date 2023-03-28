097b3714923978344e68a19b75dc3990a8f43083
Sonun <usubalievasonun@gmail.com>
2023-03-28 20:58:12 +0600

    Initial Commit

---
+++ HomeTask03-7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeTask03-7
+//
+//  Created by Sonun on 28/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
